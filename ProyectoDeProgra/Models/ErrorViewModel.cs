a3f11c1c8ca1ccfb31b5e1f791516dd74958005a
Mariana Gomez <marianagomez24123@gmail.com>
2024-08-02 17:48:41 -0600

    Inicio de Proyecto

---
+++ ProyectoDeProgra/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoDeProgra.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
